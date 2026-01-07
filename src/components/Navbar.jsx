2789f8927f43afbb6675906b06378176a1dad7e0
surender <surenderthakur40437@gmail.com>
2026-01-08 00:05:54 +0530

    finally

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 
 const sections = [
@@ -47,6 +48,36 @@ const Navbar = () => {
           onClick={() => scrollToSection("home")}
           className="cursor-pointer"
         >
+
+        <motion.div
+      initial={{ y: 0 }}
+      animate={{ y: [0, -6, 0] }}
+      transition={{
+        duration: 2.5,
+        repeat: Infinity,
+        ease: "easeInOut",
+      }}
+      className="flex items-center gap-2 cursor-pointer select-none"
+    >
+      {/* Logo Icon */}
+      <div
+        className="w-10 h-10 rounded-xl flex items-center justify-center
+                   bg-gradient-to-br from-orange-500 to-pink-500
+                   text-white font-bold text-lg shadow-lg"
+      >
+        SK
+      </div>
+
+      {/* Name */}
+      <span className="text-white font-semibold tracking-wide">
+        Surender
+      </span>
+    </motion.div>
+
+
+
+
+
           
         </div>
 
