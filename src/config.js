ac93a9e5bf2a74a6a54f7c923b758950746fd1df
surender <surenderthakur40437@gmail.com>
2026-01-16 16:00:00 +0530

    admin

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = "https://portfolio-backend-8genoqewh-surender-kumars-projects-3bd700bf.vercel.app/api";
+const API_BASE_URL = "https://backendportfolio-self.vercel.app/api";
 
 export default API_BASE_URL;
