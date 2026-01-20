4e039bce109e1e3ee4c2d7f14209488f1e147ccc
surender <surenderthakur40437@gmail.com>
2026-01-20 22:54:09 +0530

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Admin from "./pages/Admin";
-import Profile from "./pages/Profile";
-import AdminContacts from "./pages/AdminContacts";
-import AdminLogin from "./pages/AdminLogin";
-import RequireAuth from "./components/RequireAuth";
 
 
 const App = () => {
@@ -13,16 +8,6 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<AdminLogin />} />
-        <Route path="/login" element={<AdminLogin />} />
-
-        {/* Protected Routes */}
-        <Route element={<RequireAuth />}>
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/admin/profile" element={<Profile />} />
-          <Route path="/admin/contacts" element={<AdminContacts />} />
-        </Route>
-
       </Routes>
     </BrowserRouter>
   );
