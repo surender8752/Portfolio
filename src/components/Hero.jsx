ab1e2ca698150d7d3159de572d012e32d38f5484
surender <surenderthakur40437@gmail.com>
2026-01-29 19:05:25 +0530

    s

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -153,14 +153,15 @@ const Hero = () => {
                   >
                     <a
                       href="/sk_fronten_new.pdf"
-                      download="Surender_Kumar_MERN_Resume.pdf"
+                      download="sk_fronten_new.pdf"
                       className="
                         block px-4 py-3 text-sm text-gray-300
                         hover:bg-orange-500/20 hover:text-orange-500
                         transition border-b border-gray-700
                       "
                     >
-                      📄 MERN Stack Resume
+                      📄 Frontend
+                      Resume
                     </a>
                     <a
                       href="/resume_backend.pdf"
